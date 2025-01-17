7dd852f57495407d8b52d6af04278e2e54e0c912
HeisenBerg? <admin@tiktokastudio.com>
2025-01-18 05:14:45 +0800

    Create sougou.py

---
+++ sougou.py
@@ -0,0 +1,8 @@
+
+
+url='https://weixin.sogou.com/weixin?type=2&s_from=input&query=%E5%9B%BD%E4%BA%A7ct&ie=utf8&_sug_=y&_sug_type_=&w=01019900&sut=4315&sst0=1737143307348&lkt=1%2C1737143307246%2C1737143307246'
+url='https://weixin.sogou.com/weixin?type=2&s_from=input&query=%E5%9B%BD%E4%BA%A7mri&ie=utf8&_sug_=y&_sug_type_=&w=01019900&sut=8683&sst0=1737143413203&lkt=4%2C1737143411545%2C1737143413093'
+
+
+
+
